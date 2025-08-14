3d2b3020ca0915d505336b307beaea93c11fe88d
Mariful-Islam <marifulesgiu@gmail.com>
2025-08-14 11:49:48 +0600

    cors fix3

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ const allowedOrigins = ['*']
  
 const corsOptions = {
   'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization, x-api-secret',
   'Access-Control-Allow-Origin': '*'
 }
  
